d578b0d0239df4a2018206d8d5c6489109d9e96e
beauxdelcambre <beaux@stingphotodesign.com>
2021-10-25 09:46:31 -0500

    initial commit

---
+++ challenge-has-more-vowels/hasMoreVowels.js
@@ -0,0 +1,10 @@
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
+function hasMoreVowels(word){
+  for(let i = 0; i < vowels.length; i++) {
+    if(vowels.includes(word[i])) {
+      counts++;
+    }
+  }
+  return true
+}
